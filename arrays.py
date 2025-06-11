e3e5456a9599c4878b170c8b3a27f17535fac7bb
Ernesto Velásquez <34869177+ErnestoLeonidas@users.noreply.github.com>
2025-06-10 21:45:17 -0400

    actualizacion

--- arrays.py
+++ arrays.py
@@ -3,10 +3,11 @@ os.system("cls")
 
 animal = 'perro'
             # 0         1       2
-animales = [animal, 'gato', 'hamster']
+animales = [animal, 'gato', 'hamster', 'Canguro']
 
 animales.append('Gallina')
 
 # print(animales[2])
 for i in animales:
     print(i)
+
